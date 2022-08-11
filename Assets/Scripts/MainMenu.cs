a6d67e415e2550a2faa5021ffdd3fc837b870494
fireguy188 <aryehtys@gmail.com>
2022-08-11 16:57:37 +0100

    Made everything into resources folder. Added popup for no username

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
@@ -20,13 +21,51 @@ public class MainMenu : MonoBehaviour {
 
    [SerializeField] private TMP_InputField usernameField;
    [SerializeField] private TMP_InputField IPField;
+   [SerializeField] private Canvas canvas;
+   private GameObject popup;
 
    private void Awake() {
       Singleton = this;
+      SetupPopup();
+   }
+
+   private void SetupPopup() {
+      popup = Instantiate(Resources.Load("Prefabs/Popup") as GameObject);
+      popup.SetActive(false);
+      popup.transform.SetParent(canvas.transform, false);
+      
+      // Make ok button functional
+      Transform okBtn = popup.transform.Find("okBtn");
+      okBtn.gameObject.GetComponent<Button>().onClick.AddListener(PopupHandler);
+   }
+
+   private void PopupHandler() {
+      // This method is called when the popup is closed
+
+      // Make the popup inactive
+      popup.SetActive(false);
+
+      // Make all inputs and buttons interactable again
+      canvas.GetComponent<CanvasGroup>().interactable = true;
    }
 
    public void StartClicked() {
       string IP = IPField.GetComponent<TMP_InputField>().text;
+      string username = usernameField.GetComponent<TMP_InputField>().text;
+
+      // Make sure user has not left username blank
+      if (string.IsNullOrEmpty(username)) {
+         // Make all inputs and buttons greyed out
+         canvas.GetComponent<CanvasGroup>().interactable = false;
+
+         // Display the popup
+         popup.SetActive(true);
+         Transform errorText = popup.transform.Find("ErrorText");
+         errorText.GetComponent<TextMeshProUGUI>().SetText("You have not entered a username");
+
+         return;
+      }
+
       if (string.IsNullOrEmpty(IP)) {
          // This player wants to be a host
          NetworkManager.Singleton.StartHost();
@@ -34,6 +73,9 @@ public class MainMenu : MonoBehaviour {
          // This player wants to be a client
          NetworkManager.Singleton.JoinGame(IP);
       }
+
+      // Make sure user doesn't click anything else while connecting
+      canvas.GetComponent<CanvasGroup>().interactable = false;
    }
 
    public void ExitClicked() {
